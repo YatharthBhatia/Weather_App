6211abcf78d7c0cd38f7eb0c7071e3c35be0055c
YatharthBhatia <124282341+YatharthBhatia@users.noreply.github.com>
2023-10-02 23:51:23 +0530

    Add files via upload

---
+++ lib/consts/strings.dart
@@ -0,0 +1,4 @@
+const String degree = "°";
+const String apiKey = "5b5f0e891d530a6b5c8c22f030103529";
+const String currentEndpoint = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}";
+const String daysEndpoint = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
