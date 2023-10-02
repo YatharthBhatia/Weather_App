34faa4dd9117c7080e071ce6f92da7fdc5785ab6
YatharthBhatia <124282341+YatharthBhatia@users.noreply.github.com>
2023-10-02 23:52:54 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\APP DEV\\Flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\APP DEV\\Flutter_Weather_App-master" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\APP DEV\\Flutter\\flutter"
+  "PROJECT_DIR=D:\\APP DEV\\Flutter_Weather_App-master"
+  "FLUTTER_ROOT=D:\\APP DEV\\Flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\APP DEV\\Flutter_Weather_App-master\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\APP DEV\\Flutter_Weather_App-master"
+  "FLUTTER_TARGET=D:\\APP DEV\\Flutter_Weather_App-master\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9iOGQzNTgxMGU5MWFiOGZjMzliYTVlN2E0MWJmZjZmNjk3ZThlM2E4Lw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\APP DEV\\Flutter_Weather_App-master\\.dart_tool\\package_config.json"
+)
